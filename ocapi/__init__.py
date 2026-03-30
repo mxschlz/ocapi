a107b466508ed077941bfd724d686dd4a3bf603c
Max <schulz.max5@gmail.com>
2026-03-30 15:22:03 +0200

    Changes data path to OCAPI.

--- ocapi/__init__.py
+++ ocapi/__init__.py
@@ -3,9 +3,9 @@ import os
 def get_data_path():
   """Returns the correct file path to the EEG data based on the OS."""
   if os.name == 'nt':  # Windows
-    data_path = 'G:\\Meine Ablage\\PhD\\data\\BABYGAZE\\'
+    data_path = 'G:\\Meine Ablage\\PhD\\data\\OCAPI\\'
   elif os.name == 'posix':  # Linux
-    data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/BABYGAZE/'
+    data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/OCAPI/'
   else:
     raise OSError("Unsupported operating system.")
   return data_path
